810d06c9e23a942e7de6e6880885ae6d77da4089
Malcom Terry <malcom.terry@spirent.com>
2018-03-06 16:15:38 -0600

    Now supports RedHat as well as Ubuntu

--- Libraries/Tools/us_flexlm_tools/flexlm_check.tcl
+++ Libraries/Tools/us_flexlm_tools/flexlm_check.tcl
@@ -1,18 +1,40 @@
+set distro unknown
+set response [exec gcc --version]
+if {[string first "(Red Hat" $response] > -1} {
+	set distro "RedHat"
+} elseif {[string first "(Ubuntu" $response] > -1} {
+	set distro "Ubuntu"
+}
+if {$distro == "unknown"} {
+	puts "ERROR: Unknown Linux distribution. Expected RedHat or Ubuntu"
+	exit
+}
 if {![catch {exec ./lmutil lmver ./lmutil} result]} { 
 	puts [lindex [split $result \n] 1]		
 } else {
-	if {[string match "*error while loading shared libraries: libgcc_s.so.1:*" $result]} {	
-		puts "ERROR:Somthing is missing!"
-		puts "Try executing the following:"
-		puts "  sudo apt-get update"
-		puts "  sudo apt-get install -y libc6:i386 libgcc1:i386 libstdc++6:i386 libz1:i386"	
-	} elseif {[string match "*no such file or directory*" $result]} {
-		puts "ERROR:Somthing is missing!"
-		puts "Try executing the following:"
-		puts "  sudo apt-get update"
-		puts "  sudo apt-get install lsb-core"	
+	if {$distro == "Ubuntu"} {
+		if {[string match "*error while loading shared libraries: libgcc_s.so.1:*" $result]} {	
+			puts "ERROR:Something is missing!"
+			puts "Try executing the following:"
+			puts "  sudo apt-get update"
+			puts "  sudo apt-get install -y libc6:i386 libgcc1:i386 libstdc++6:i386 libz1:i386"	
+		} elseif {[string match "*no such file or directory*" $result]} {
+			puts "ERROR:Something is missing!"
+			puts "Try executing the following:"
+			puts "  sudo apt-get update"
+			puts "  sudo apt-get install lsb-core"	
+		} else {
+			puts "ERROR: $result"
+		}
 	} else {
-		puts "ERROR: $result"
+		if {[string match "*no such file or directory*" $result]} {
+			puts "ERROR:Something is missing!"
+			puts "Try executing the following:"
+			puts "  su"	
+			puts "  yum -y install redhat-lsb.i686 redhat-lsb-graphics.i686 redhat-lsb-printing.i686"	
+		} else {
+			puts "ERROR: $result"
+		}
 	}
 	exit
 }
@@ -21,15 +43,26 @@ if {![file isdirectory /usr/tmp]} {
 	puts "ERROR:Folder not found: /usr/tmp"
 	puts "spirentd (the vendor daemon) requires read/write access to this location"
 	puts "Try executing the following:"
-	puts "  sudo mkdir /usr/tmp"
-	puts "  sudo chmod 777 /usr/tmp"
+	if {$distro == "Ubuntu"} {
+		puts "  sudo mkdir /usr/tmp"
+		puts "  sudo chmod 777 /usr/tmp"
+	} else {
+		puts "  su"
+		puts "  mkdir /usr/tmp"
+		puts "  chmod 777 /usr/tmp"
+	}
 } else {
 	set perms [file attributes /usr/tmp -permissions]
 	if {![string match "???777" $perms]} {
 		puts "ERROR:Insufficient permissions for folder: /usr/tmp"
 		puts "spirentd (the vendor daemon) requires read/write access to this location"
 		puts "Try executing the following:"
-		puts "  sudo chmod 777 /usr/tmp"
+		if {$distro == "Ubuntu"} {
+			puts "  sudo chmod 777 /usr/tmp"
+		} else {
+			puts "  su"
+			puts "  chmod 777 /usr/tmp"
+		}
 	}
 }
 
@@ -38,7 +71,12 @@ if {[catch {exec ./lmutil lmhostid -ptype VM} result]} {
 		puts "VM UUID file not readable:"
 		puts "You will not be able to get the UUID for a license until the file permissions are corrected."
 		puts "Try executing the following:"
-		puts "  sudo chmod 666 /sys/devices/virtual/dmi/id/product_uuid"
+		if {$distro == "Ubuntu"} {
+			puts "  sudo chmod 666 /sys/devices/virtual/dmi/id/product_uuid"
+		} else {
+			puts "  su"
+			puts "  chmod 666 /sys/devices/virtual/dmi/id/product_uuid"
+		}
 	} else {
 		puts "ERROR:Could not get UUID"
 		puts "You will not be able to get the UUID for a license until the problem is corrected."
